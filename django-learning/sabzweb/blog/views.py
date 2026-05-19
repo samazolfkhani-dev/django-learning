5a983eb705a969f207988cbdcf84aba41b614f98
Sama Zolfkhani <samazolfkhani.work@gmail.com>
2026-05-19 07:13:15 -0700

    Add Some Front

--- django-learning/sabzweb/blog/views.py
+++ django-learning/sabzweb/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , get_object_or_404
 from django.http import HttpResponse ,Http404
 from . models import Post
+from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
@@ -8,6 +9,9 @@ def index(request):
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(posts , 2)
+    page_number = request.GET.get('page' , 1)
+    posts = paginator.page(page_number)
     context = {
         'posts' : posts,
     }
