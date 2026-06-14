4f7ab25f31b6931d2c93d956248d320ac5ab7217
Sama Zolfkhani <samazolfkhani.work@gmail.com>
2026-06-14 02:51:09 -0700

    Add Inline To Post Model

--- django-learning/sabzweb/blog/admin.py
+++ django-learning/sabzweb/blog/admin.py
@@ -4,6 +4,18 @@ from django_jalali.admin.filters import JDateFieldListFilter
 import django_jalali.admin as jadmin
 # Register your models here.
 
+#inlines
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+    fields = ['body']
+    readonly_fields = ['name' , 'created' , 'body']
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 0
+    readonly_fields = ['created']
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = [ 'title' , 'author' , 'publish' , 'status' ]
@@ -15,6 +27,7 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug' : ['title']}
     list_editable = ['status' ]
     list_display_links = ['title' , 'author']
+    inlines = [ CommentInline , ImageInline ]
 
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
