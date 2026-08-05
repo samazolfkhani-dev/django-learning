cc9a780cb95a98fdfe0c3913d1ddc0b30c03db76
samazolfkhani-dev <samazolfkhani.work@gmail.com>
2026-08-05 17:26:19 +0330

    Change Database SQL Into Postgresql

--- sabzsocial/sabzsocial/sabzsocial/settings.py
+++ sabzsocial/sabzsocial/sabzsocial/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'sabzsocial.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "social_db",
+        "USER": "social_admin",
+        "PASSWORD": "samazolfi1385",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
