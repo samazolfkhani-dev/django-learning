880306c2bcfcffff443d28545dbcfd6e1ff44f74
samazolfkhani-dev <samazolfkhani.work@gmail.com>
2026-09-13 18:33:28 +0330

    Enable Adding Product To Cart By Ajax

--- sabzshop/sabzshop/cart/views.py
+++ sabzshop/sabzshop/cart/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
+from django.views.decorators.http import require_POST
+from shop.models import Product
+from .cart import Cart
+from django.http import JsonResponse
 
 # Create your views here.
+
+def add_to_cart(request , product_id) :
+    try :
+        product = get_object_or_404(Product , id = product_id)
+        cart = Cart(request)
+        cart.add(product)
+        context ={
+            'item_count' : len(cart) ,
+            'total_price' : cart.get_total_price(),
+        }
+        return JsonResponse(context)
+    except :
+        return JsonResponse({'error' : 'Invalid Request!'})
+
--- sabzshop/sabzshop/sabzshop/urls.py
+++ sabzshop/sabzshop/sabzshop/urls.py
@@ -21,7 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , include('shop.urls' , namespace='shop'))
+    path('' , include('shop.urls' , namespace='shop')) ,
+    path('cart/' , include('cart.urls' , namespace='cart'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
