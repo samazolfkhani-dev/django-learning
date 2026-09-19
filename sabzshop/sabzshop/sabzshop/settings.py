639a1ddd5ece8a5499545f3a59eb51a4420e5aab
samazolfkhani-dev <samazolfkhani.work@gmail.com>
2026-09-19 09:47:40 +0330

    Add Order App

--- sabzshop/sabzshop/sabzshop/settings.py
+++ sabzshop/sabzshop/sabzshop/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
-    'cart.apps.CartConfig'
+    'cart.apps.CartConfig',
+    'order.apps.OrderConfig',
 ]
 
 MIDDLEWARE = [
